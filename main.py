61a394af9af5d248773d4c525e75f543cabf42f2
Angela-Sanchez-Guitart <ansanguit@gmail.com>
2021-05-18 16:48:53 +0100

    chore: Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+BACKGROUND_COLOR = "#B1DDC6"
+from tkinter import *
+
+#---------------CREATE USER INTERFACE-------------------
+
+window =Tk()
+window.title("Flash Card")
+window.config(padx = 50, pady= 50, width= 800 , height=525, bg = BACKGROUND_COLOR)
+
+
+
+
+window.mainloop()
+
+
+
